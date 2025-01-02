03bdfd8c0a542a0c5f6a2f7d548f91800216ae92
Daniel Molenda <daniel.molenda@shoper.pl>
2025-01-02 15:35:11 +0100

    Added example program

---
+++ example-program.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+word = input("Please enter a word: ").lower()
+
+vowels_list = ["a", "e", "i", "o", "u"]
+vowels = 0
+consonants = 0
+
+for letter in word:
+    if letter.isalpha():
+        if letter in vowels_list:
+            vowels += 1
+        else:
+            consonants += 1
+
+print("Number of vowels:", vowels)
+print("Number of consonants:", consonants)
